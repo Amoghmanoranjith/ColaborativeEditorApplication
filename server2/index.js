55ba39a7d53f53b40868c4282b43252540443f8c
AMOGH NAVADE <amoghmanoranjith@gmail.com>
2025-07-18 22:27:14 +0530

    updated server to fetch from rapidapi

--- server2/index.js
+++ server2/index.js
@@ -3,23 +3,16 @@ import http from "http";
 import { WebSocketServer } from "ws";
 import { setupWSConnection } from "@y/websocket-server/utils";
 import cors from "cors"
-// Setup express app
+import axios from 'axios';
+
 const app = express();
-app.use(express.json()); // middleware to parse JSON body
+app.use(express.json());
 app.use(cors())
-// Create HTTP server from express app
 const server = http.createServer(app);
- 
-// In-memory store for room data
+
 const roomData = new Map();
-/*
-Structure:
-roomName: {
-  admin: string,
-  language_id: number,
-}
-*/
- 
+
+
 // Create room endpoint
 app.post('/create-room', (req, res) => {
   const { roomName, admin, language_id } = req.body;
@@ -43,8 +36,61 @@ app.get('/join-room/:roomName', (req, res) => {
   const roomName = req.params.roomName;
   const roomMeta = roomData.get(roomName);
   if (!roomMeta) return res.status(404).json({ error: "Room not found." });
-  res.status(200).json({...roomMeta});
+  res.status(200).json({ ...roomMeta });
 });
+
+app.post('/compile', async (req, res) => {
+  try {
+    const { language_id, source_code, stdin } = req.body;
+    const options = {
+      method: 'POST',
+      url: 'https://judge0-ce.p.rapidapi.com/submissions',
+      params: {
+        base64_encoded: 'true',
+        wait: 'false',
+        fields: '*'
+      },
+      headers: {
+        'x-rapidapi-key': '3c80a113c8mshb48590a41fe6e12p14f11fjsn3e5e0f787c85',
+        'x-rapidapi-host': 'judge0-ce.p.rapidapi.com',
+        'Content-Type': 'application/json'
+      },
+      data: {
+        language_id: language_id,
+        source_code: btoa(source_code),
+        stdin: btoa(stdin)
+      }
+    };
+    const response = await axios.request(options);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+app.get('/status', async (req, res) => {
+  try {
+    const { token } = req.body;
+    const options = {
+      method: 'GET',
+      url: `https://judge0-ce.p.rapidapi.com/submissions/${token}`,
+      params: {
+        base64_encoded: 'true',
+        fields: '*'
+      },
+      headers: {
+        'x-rapidapi-key': '3c80a113c8mshb48590a41fe6e12p14f11fjsn3e5e0f787c85',
+        'x-rapidapi-host': 'judge0-ce.p.rapidapi.com'
+      }
+    }
+    const response = await axios.request(options);
+    response.stdout = atob(response.stdout);
+    response.stderr = atob(response.stderr);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+})
 
 const wss = new WebSocketServer({ server });
 
